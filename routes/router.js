77bb922e1f2c97a06d91e1775bc91b603f20eb2a
AleAlves <aleson.a.s@gmail.com>
2019-03-06 20:19:32 -0300

    ajuste nas rotas

--- routes/router.js
+++ routes/router.js
@@ -10,8 +10,8 @@ module.exports = (app) => {
   app.get(api_v1 + '/version', ServiceController.version);
 
   //Login
-  app.get('/init',LoginController.init);
-  app.post('/ticket',LoginController.ticket);
-  app.post('/login',LoginController.login);
-  
+  app.get(api_v1 + '/init', LoginController.init);
+  app.post(api_v1 + '/ticket', LoginController.ticket);
+  app.post(api_v1 + '/login', LoginController.login);
+
 }
